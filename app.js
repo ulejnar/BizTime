1105d4f3da16e35e5063dbcdf5f8bc32073c8620
ulejnar <ulejnar@yahoo.de>
2020-04-09 15:48:26 -0700

    first part completed

--- app.js
+++ app.js
@@ -18,14 +18,26 @@ app.use(function(req, res, next) {
 
 /** general error handler */
 
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+// app.use((err, req, res, next) => {
+//   res.status(err.status || 500);
 
-  return res.json({
-    error: err,
-    message: err.message
+//   return res.json({
+//     error: err,
+//     message: err.message
+//   });
+// });
+
+app.use(function(err, req, res, next) {
+  // the default status is 500 Internal Server Error
+  let status = err.status || 500;
+
+  // set the status and alert the user
+  return res.status(status).json({
+    error: {
+      message: err.message,
+      status: status
+    }
   });
 });
-
 
 module.exports = app;
--- routes/companies.js
+++ routes/companies.js
@@ -3,7 +3,8 @@
 
 const express = require("express");
 const router = new express.Router();
-
+const ExpressError = require("../expressError")
+const db = require("../db")
 const companies = []
 
 // at localhost:3000/companies
@@ -21,17 +22,75 @@ router.get("/", async function (req, res, next) {
     }
 })
 
-// router.get("/companies/:code", async function (req, res, next) {
-//     try {
-//         const results = await db.query(
-//             `SELECT code, name, description
-//             FROM `
-//         )
-//     }
-//     catch{
+router.get("/:code", async function (req, res, next) {
+    try {
+        const results = await db.query(
+            `SELECT code, name, description
+            FROM companies
+            WHERE code = $1`, [req.params.code]);
 
-//     }
-// })
+    if (results.rows.length === 0) {
+        let notFoundError = new Error(`There is no compnay with code '${req.params.code}`);
+        notFoundError.status = 404;
+        throw notFoundError;
+      }
+      return res.json({ company: results.rows[0] });
+    } catch (err) {
+      return next(err);
+    }
+})
+
+router.post("/", async function (req, res, next){
+    try{
+        console.log(req.body);
+        const result = await db.query(
+            `INSERT INTO companies (code, name, description) 
+               VALUES ($1, $2, $3) 
+               RETURNING code, name, description`,
+            [req.body.code, req.body.name, req.body.description]);
+        return res.status(201).json({company: result.rows[0]});
+    }
+    catch (err) {
+        return next(err);
+      }
+})
+
+router.put("/:code", async function (req, res, next){
+    try {
+        if ("code" in req.body) {
+          throw new ExpressError("Not allowed", 400)
+        }
+    
+        const result = await db.query(
+          `UPDATE companies 
+               SET name = $1, description = $2
+               WHERE code = $3
+               RETURNING code, name, description`,
+          [req.body.name, req.body.description, req.params.code]);
+    
+        if (result.rows.length === 0) {
+          throw new ExpressError(`There is no company with code of '${req.params.code}`, 404);
+        }
+    
+        return res.json({ company: result.rows[0]});
+      } catch (err) {
+        return next(err);
+      }
+})
+
+router.delete("/:code", async function(req, res, next){
+    try {
+        const result = await db.query(
+          "DELETE FROM companies WHERE code = $1 RETURNING code", [req.params.code]);
+    
+        if (result.rows.length === 0) {
+          throw new ExpressError(`There is no company with code of '${req.params.code}`, 404);
+        }
+        return res.json({ status: "deleted" });
+      } catch (err) {
+        return next(err);
+      }
+})
 
 
 module.exports = router;
